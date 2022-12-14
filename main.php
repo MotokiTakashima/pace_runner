baea919e895898600da322c2d6ddf8a6a757e402
zyyx-m-takashima <m-takashima@zyyx.jp>
2022-12-14 23:05:23 +0900

    main.phpに追加

--- main.php
+++ main.php
@@ -3,6 +3,8 @@ $distance = $_POST['distance'];
 $pace = $_POST['pace'];
 $paceDistance = $_POST['paceDistance'];
 $targetTime = $_POST['targetTime'];
+$time = $pace * $paceDistance;
+var_dump($time);
 echo "総距離", $distance, "km。";
 echo "ペース、km/", $distance, "分";
 echo "総距離", $distance, "km。";
