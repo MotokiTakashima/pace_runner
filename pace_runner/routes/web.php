b0363ad328989b0126f956da2e70904e9130d720
zyyx-m-takashima <m-takashima@zyyx.jp>
2022-12-16 00:07:48 +0900

    inputをtime

--- pace_runner/routes/web.php
+++ pace_runner/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('index', PaceController::class, ['except' => ['show']]);
+Route::resource('/index', PaceController::class, 'index');
