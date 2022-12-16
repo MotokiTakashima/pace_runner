ea2e15f2545b1eb9239eba0dc25d3708adc40709
zyyx-m-takashima <m-takashima@zyyx.jp>
2022-12-16 11:11:00 +0900

    ゴールタイムの計算処理

--- main.php
+++ main.php
@@ -6,9 +6,17 @@ $date = $pace * $distance;
 $date = date("Y/m/d H:i:s",$pace);
 $paceDistance = $_POST['paceDistance'];
 $targetTime = $_POST['targetTime'];
+$goalTime = MultTime($pace,$distance);
 $time = $pace * $paceDistance;
-var_dump($time);
-echo "総距離", $distance, "km。";
-echo "ペース、km/", $distance, "分";
-echo "総距離", $distance, "km。";
+echo "総距離 ", $distance, "km。<br>";
+echo "ペース、km/", $distance, "分<br>";
+echo "タイム ", $goalTime, "km。<br>";
+function MultTime($time,$kake){
+    $tArry=explode(":",$time);
+    $secnd=round($tArry[1]/60,2);//秒→分
+    $mins=$tArry[0]+$secnd;
+    $ans= $mins*$kake*60;//割合いを掛け算して秒に変換
+    
+    return date("H:i:s",mktime(0,0,$ans));
+    }
 ?>
