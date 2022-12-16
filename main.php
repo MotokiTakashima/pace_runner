79a64c10d2b6f6fd30fc6da9038b316771309802
zyyx-m-takashima <m-takashima@zyyx.jp>
2022-12-16 10:25:50 +0900

    時間を分ける処理

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 <?php
 $distance = $_POST['distance'];
 $pace = $_POST['pace'];
+$tArry = explode(":",$time);
+$date = $pace * $distance;
+$date = date("Y/m/d H:i:s",$pace);
 $paceDistance = $_POST['paceDistance'];
 $targetTime = $_POST['targetTime'];
 $time = $pace * $paceDistance;
