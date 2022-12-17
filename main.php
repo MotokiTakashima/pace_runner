0e3e5d3de3be43772d6169809ea5be10b82bfb63
zyyx-m-takashima <m-takashima@zyyx.jp>
2022-12-17 17:25:24 +0900

    目標タイムを時間に変更

--- main.php
+++ main.php
@@ -9,6 +9,18 @@ $tArry = explode(":",$time);
 $paceDistance = $_POST['paceDistance'];
 // 目標タイム
 $targetTime = $_POST['targetTime'];
+// 目標タイムを秒に変換
+if ($targetTime = 3) {
+    $seconds = 10800;
+}elseif($targetTime = 3.5) {
+    $seconds = 12600;
+}elseif($targetTime = 4) {
+    $seconds = 14400;
+}elseif($targetTime = 4.5) {
+    $seconds = 16200;
+}elseif($targetTime = 5) {
+    $seconds = 18000;
+}
 // ゴールタイム
 $goalTime = MultTime($pace,$distance);
 // 総距離から維持するペースの距離を引いた残り
@@ -18,6 +30,7 @@ echo "総距離 ", $distance, "km。<br><br>";
 echo "ペース、km/", $pace, "分<br>";
 echo "何キロまで？ ", $paceDistance, "km。<br>";
 
+echo "残り距離 ", $residue, "km。<br><br>";
 echo "目標タイム ", $targetTime, "時間切り<br>";
 echo "タイム ", $goalTime, "<br>";
 function MultTime($time,$kake){
